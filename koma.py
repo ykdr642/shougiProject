bd213fbbcb1258ff46cf802c63433c9381d27365
Usho <taken517@gmail.com>
2018-11-25 16:45:43 +0900

    Make the flag of firstTurn or secondTurn

--- koma.py
+++ koma.py
@@ -1,15 +1,19 @@
 class koma:
-    def __init__(self,movability):
-        self.name = ""
-        self.reverseName = ""
+    def __init__(self,name,rename,firstTurn,movability):
+        self.name = name
+        self.reverseName = rename
         self.xposition = -1
         self.yposition = -1
-        self.firstTurn = True
-        self.komaChange = True
+        self.firstTurn = firstTurn
+        self.komaChange = False
         self.movablity = movability
 
     def check(self,movablity:list):
         for a in self.movablity:
-            if a[0] == movablity[0] and a[1] == movablity[1]:
-                return True
+            if self.firstTurn:
+                if a[0] == -movablity[0] and a[1] == -movablity[1]:
+                    return True
+            else:
+                if a[0] == movablity[0] and a[1] == movablity[1]:
+                    return True
         return False
