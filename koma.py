051050c0ea89cba4dc3cd4e6ec67aab53b9fb181
Usho Tei <taken517@gmail.com>
2019-03-19 19:20:27 +0900

    風俗行き過ぎてお金がない

--- koma.py
+++ koma.py
@@ -1,19 +1,360 @@
-class koma:
-    def __init__(self,name,rename,firstTurn,movability):
+
+
+class Koma:
+    def __init__(self,name,rename,first,movability):
         self.name = name
         self.reverseName = rename
         self.xposition = -1
         self.yposition = -1
-        self.firstTurn = firstTurn
+        self.first = first
         self.komaChange = False
-        self.movablity = movability
+        self.movablity = movablity
 
     def check(self,movablity:list):
         for a in self.movablity:
-            if self.firstTurn:
+            if self.first:
                 if a[0] == -movablity[0] and a[1] == -movablity[1]:
                     return True
             else:
                 if a[0] == movablity[0] and a[1] == movablity[1]:
                     return True
         return False
+
+
+class Fu:
+    def __init__(self,first):
+        self.name = "歩"
+        self.reverseName = "と"
+        self.xposition = None
+        self.yposition = None
+        self.first = first
+        self.komaChange = False
+        self.movablity = [[0,1]]
+        self.reMovablity = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,1]]
+
+    def run(self,pos):
+        if not self.komaChange:
+            return self.check(pos)
+        else:
+            return self.reCheck(pos)
+
+    def check(self,pos):
+        for a in self.movablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reCheck(self,pos):
+        for a in self.reMovablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reverse(self):
+        if self.komaChange:
+            self.komaChange = False
+        else:
+            self.komaChange = True
+
+class Kyosya:
+    def __init__(self,first):
+        self.name = "香車"
+        self.reverseName = "成香"
+        self.xposition = None
+        self.yposition = None
+        self.first = first
+        self.komaChange = False
+        self.movablity = [[0,1],[0,2],[0.3],[0,4],[0,5],[0,6],[0,7],[0,8]]
+        self.reMovablity = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,1]]
+
+    def run(self,pos):
+        if not self.komaChange:
+            return self.check(pos)
+        else:
+            return self.reCheck(pos)
+
+    def check(self,pos):
+        for a in self.movablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reCheck(self,pos):
+        for a in self.reMovablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reverse(self):
+        if self.komaChange:
+            self.komaChange = False
+        else:
+            self.komaChange = True
+
+class Keima:
+    def __init__(self,first):
+        self.name = "桂馬"
+        self.reverseName = "成桂"
+        self.xposition = None
+        self.yposition = None
+        self.first = first
+        self.komaChange = False
+        self.movablity = [[1,2],[-1,2]]
+        self.reMovablity = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,1]]
+
+    def run(self,pos):
+        if not self.komaChange:
+            return self.check(pos)
+        else:
+            return self.reCheck(pos)
+
+    def check(self,pos):
+        for a in self.movablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reCheck(self,pos):
+        for a in self.reMovablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reverse(self):
+        if self.komaChange:
+            self.komaChange = False
+        else:
+            self.komaChange = True
+
+class Ginsyo:
+    def __init__(self,first):
+        self.name = "銀将"
+        self.reverseName = "成銀"
+        self.xposition = None
+        self.yposition = None
+        self.first = first
+        self.komaChange = False
+        self.movablity = [[0,1],[1,1],[1,-1],[1,-1],[-1,-1]]
+        self.reMovablity = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,1]]
+
+    def run(self,pos):
+        if not self.komaChange:
+            return self.check(pos)
+        else:
+            return self.reCheck(pos)
+
+    def check(self,pos):
+        for a in self.movablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reCheck(self,pos):
+        for a in self.reMovablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reverse(self):
+        if self.komaChange:
+            self.komaChange = False
+        else:
+            self.komaChange = True
+
+class Kinsyo:
+    def __init__(self,first):
+        self.name = "金将"
+        self.reverseName = None
+        self.xposition = None
+        self.yposition = None
+        self.first = first
+        self.komaChange = False
+        self.movablity = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,1]]
+        self.reMovablity = None
+
+    def run(self,pos):
+        if not self.komaChange:
+            return self.check(pos)
+        else:
+            return self.reCheck(pos)
+
+    def check(self,pos):
+        for a in self.movablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reCheck(self,pos):
+        for a in self.reMovablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reverse(self):
+        pass
+
+class Kakugyo:
+    def __init__(self,first):
+        self.name = "角行"
+        self.reverseName = "龍馬"
+        self.xposition = None
+        self.yposition = None
+        self.first = first
+        self.komaChange = False
+        self.movablity = [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8],[1,-1],[2,-2],[3,-3],[4,-4],[5,-5],[6,-6],[7,-7],[8,-8],[-1,1],[-2,2],[-3,3],[-4,4],[-5,5],[-6,6],[-7,7],[-8,8],[-1,-1],[-2,-2],[-3,-3],[-4,-4],[-5,-5],[-6,-6],[-7,-7],[-8,-8]]
+        self.reMovablity =[[1,0],[-1,0],[0,1],[0,-1],[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8],[1,-1],[2,-2],[3,-3],[4,-4],[5,-5],[6,-6],[7,-7],[8,-8],[-1,1],[-2,2],[-3,3],[-4,4],[-5,5],[-6,6],[-7,7],[-8,8],[-1,-1],[-2,-2],[-3,-3],[-4,-4],[-5,-5],[-6,-6],[-7,-7],[-8,-8]]
+
+    def run(self,pos):
+        if not self.komaChange:
+            return self.check(pos)
+        else:
+            return self.reCheck(pos)
+
+    def check(self,pos):
+        for a in self.movablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reCheck(self,pos):
+        for a in self.reMovablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reverse(self):
+        if self.komaChange:
+            self.komaChange = False
+        else:
+            self.komaChange = True
+
+class Hisya:
+    def __init__(self,first):
+        self.name = "飛車"
+        self.reverseName = "龍王"
+        self.xposition = None
+        self.yposition = None
+        self.first = first
+        self.komaChange = False
+        self.movablity = [[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8],[-1,0],[-2,0],[-3,0],[-4,0],[-5,0],[-6,0],[-7,0],[-8,0],[0,-1],[0,-2],[0,-3],[0,-4],[0,-5],[0,-6],[0,-7],[0,-8]]
+        self.reMovablity =[[1,1],[1,-1],[-1,1],[-1,-1],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8],[-1,0],[-2,0],[-3,0],[-4,0],[-5,0],[-6,0],[-7,0],[-8,0],[0,-1],[0,-2],[0,-3],[0,-4],[0,-5],[0,-6],[0,-7],[0,-8]]
+
+    def run(self,pos):
+        if not self.komaChange:
+            return self.check(pos)
+        else:
+            return self.reCheck(pos)
+
+    def check(self,pos):
+        for a in self.movablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reCheck(self,pos):
+        for a in self.reMovablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reverse(self):
+        if self.komaChange:
+            self.komaChange = False
+        else:
+            self.komaChange = True
+
+class Ousyo:
+    def __init__(self,first):
+        self.name = "王将"
+        self.reverseName = None
+        self.xposition = None
+        self.yposition = None
+        self.first = first
+        self.komaChange = False
+        self.movablity = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]]
+        self.reMovablity = None
+
+    def run(self,pos):
+        if not self.komaChange:
+            return self.check(pos)
+        else:
+            return self.reCheck(pos)
+
+    def check(self,pos):
+        for a in self.movablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reCheck(self,pos):
+        for a in self.reMovablity:
+            if self.first:
+                if a[0] == -pos[0] and a[1] == -pos[1]:
+                    return True
+            else:
+                if a[0] == pos[0] and a[1] == pos[1]:
+                    return True
+        return False
+
+    def reverse(self):
+        pass
--- test_shougi.py
+++ test_shougi.py
@@ -1,11 +1,125 @@
-from koma import koma
+import koma
 
-def test_move():
-    fu = koma("歩","と",False,[[0,1]])
+def tes_koma1():
+    fu = koma.Koma("歩","と",False,[[0,1]])
     assert fu.check([0,-1]) == False
     assert fu.check([0,1]) == True
 
-def test_move2():
-    fu = koma("歩","と",True,[[0,1]])
+def tes_koma2():
+    fu = koma.Koma("歩","と",True,[[0,1]])
     assert fu.check([0,-1]) == True
     assert fu.check([0,1]) == False
+
+def test_fu1():
+    fu = koma.Fu(False)
+    assert fu.run([0,-1]) == False
+    assert fu.run([0,1]) == True
+
+def test_fu2():
+    fu = koma.Fu(True)
+    assert fu.run([0,-1]) == True
+    assert fu.run([0,1]) == False
+
+def test_kyosya1():
+    kyosya = koma.Kyosya(False)
+    assert kyosya.run([0,-1]) == False
+    assert kyosya.run([0,1]) == True
+    assert kyosya.run([1,0]) == False
+    assert kyosya.run([0,8]) == True
+    assert kyosya.run([0,9]) == False
+    kyosya.reverse()
+    assert kyosya.run([0,8]) == False
+    assert kyosya.run([0,-1]) == True
+
+def test_kyosya2():
+    kyosya = koma.Kyosya(True)
+    assert kyosya.run([0,-1]) == True
+    assert kyosya.run([0,1]) == False
+    assert kyosya.run([-1,0]) == False
+    assert kyosya.run([0,-8]) == True
+    assert kyosya.run([0,-9]) == False
+
+def test_keima1():
+    keima = koma.Keima(False)
+    assert keima.run([1,2]) == True
+    assert keima.run([-1,2]) == True
+    assert keima.run([1,-2]) == False
+    assert keima.run([-1,-2]) == False
+    keima.reverse()
+    assert keima.run([1,2]) == False
+    assert keima.run([0,-1]) == True
+
+def test_keima2():
+    keima = koma.Keima(True)
+    assert keima.run([1,2]) == False
+    assert keima.run([-1,2]) == False
+    assert keima.run([1,-2]) == True
+    assert keima.run([-1,-2]) == True
+
+def test_gin():
+    gin = koma.Ginsyo(False)
+    assert gin.run([0,1]) == True
+    assert gin.run([1,1]) == True
+    assert gin.run([1,-1]) == True
+    assert gin.run([0,-1]) == False
+    assert gin.run([1,0]) == False
+    gin.reverse()
+    assert gin.run([1,-1]) == False
+    assert gin.run([0,-1]) == True
+    assert gin.run([1,0]) == True
+
+def test_kin():
+    kin = koma.Kinsyo(False)
+    assert kin.run([0,1]) == True
+    assert kin.run([1,0]) == True
+    assert kin.run([1,-1]) == False
+    assert kin.run([-1,-1]) == False
+    kin.reverse()
+    assert kin.run([0,1]) == True
+    assert kin.run([1,0]) == True
+    assert kin.run([1,-1]) == False
+    assert kin.run([-1,-1]) == False
+
+def test_kaku():
+    kaku = koma.Kakugyo(False)
+    assert kaku.run([1,1]) == True
+    assert kaku.run([8,8]) == True
+    assert kaku.run([9,9]) == False
+    assert kaku.run([1,0]) == False
+    assert kaku.run([0,1]) == False
+    kaku.reverse()
+    assert kaku.run([1,1]) == True
+    assert kaku.run([8,8]) == True
+    assert kaku.run([9,9]) == False
+    assert kaku.run([1,0]) == True
+    assert kaku.run([0,1]) == True
+
+def test_hiysa():
+    hisya = koma.Hisya(False)
+    assert hisya.run([1,0]) == True
+    assert hisya.run([8,0]) == True
+    assert hisya.run([0,1]) == True
+    assert hisya.run([0,8]) == True
+    assert hisya.run([9,0]) == False
+    assert hisya.run([0,9]) == False
+    assert hisya.run([1,1]) == False
+    hisya.reverse()
+    assert hisya.run([1,0]) == True
+    assert hisya.run([8,0]) == True
+    assert hisya.run([0,1]) == True
+    assert hisya.run([0,8]) == True
+    assert hisya.run([9,0]) == False
+    assert hisya.run([0,9]) == False
+    assert hisya.run([1,1]) == True
+
+def test_ou():
+    ou = koma.Ousyo(False)
+    assert ou.run([0,1]) == True
+    assert ou.run([1,1]) == True
+    assert ou.run([1,2]) == False
+    assert ou.run([2,0]) == False
+    ou.reverse()
+    assert ou.run([0,1]) == True
+    assert ou.run([1,1]) == True
+    assert ou.run([1,2]) == False
+    assert ou.run([2,0]) == False
